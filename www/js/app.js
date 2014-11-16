c865df2e032f3f4819c15e219f6c5f23ffff887a
Jerome Anthony <jerome.pradeep@gmail.com>
2014-11-16 13:26:11 +1100

    Hookup leaflet map directive

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,8 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('imin', ['ionic', 'imin.services', 'imin.controllers'])
+angular.module('imin', ['ionic', 'imin.services', 'imin.controllers', 
+  'leaflet-directive'])
 
 
 .config(function($stateProvider, $urlRouterProvider) {
